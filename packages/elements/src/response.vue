6387cccca57c9fa8aa06ba7bbd232708eddb872a
fishers66 <fisherfkb@gmail.com>
2025-09-19 09:04:32 +0800

    feat: refactor task reason ui

--- packages/elements/src/response.vue
+++ packages/elements/src/response.vue
@@ -1,28 +1,49 @@
 <script setup lang="ts">
 import { cn } from '@repo/shadcn-vue/lib/utils'
-import { computed, useAttrs } from 'vue'
+import { StreamMarkdown } from 'streamdown-vue'
+import { computed, useAttrs, useSlots } from 'vue'
+import 'katex/dist/katex.min.css'
 
 interface Props {
   content?: string
-  class?: string
 }
 
 const props = defineProps<Props>()
 const attrs = useAttrs()
+const slots = useSlots()
 
 const classes = computed(() =>
   cn(
     'size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0',
-    props.class,
-    attrs.class as string || '',
+    attrs.class || '',
   ),
 )
+
+const hasNonTextSlotContent = computed(() => {
+  const nodes = slots.default?.() || []
+  return nodes.some(node =>
+    node.type !== 'text'
+    && node.type !== Symbol.for('v-txt')
+    && typeof node.children !== 'string',
+  )
+})
+
+const slotContent = computed<string | undefined>(() => {
+  const nodes = slots.default?.() || []
+  let text = ''
+  for (const node of nodes) {
+    if (typeof node.children === 'string')
+      text += node.children
+  }
+  return text || undefined
+})
+
+const md = computed(() => (slotContent.value ?? props.content ?? '') as string)
 </script>
 
 <template>
-  <div :class="classes">
-    <slot>
-      <div v-if="props.content" v-html="props.content" />
-    </slot>
+  <div v-if="hasNonTextSlotContent" :class="classes">
+    <slot />
   </div>
+  <StreamMarkdown v-else :class="classes" :content="md" />
 </template>
